075848484fc3d06b9d85815b5516791bf585b7b2
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-02-22 10:06:22 +0200

    Setup express boilerplate

---
+++ server/src/config/constants.ts
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const IS_PROD = process.env.NODE_ENV === "production";
+
+export const PORT = parseInt(<string>process.env.PORT) ?? 8080;
+
+export const MONGO_URI = IS_PROD
+  ? process.env.MONGO_URI_PROD
+  : process.env.MONGO_URI_DEV;
