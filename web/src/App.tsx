6b0733a943baa2788399d4b550d22cd2d690ef71
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-02-22 09:52:43 +0200

    Setup react boilerplate

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi maxime
+        officia suscipit ut eum distinctio nam sit ipsum, nostrum quae accusamus
+        qui sed magni ea libero debitis, fugiat, corporis dolorem!
+      </p>
+    </>
+  );
+};
+
+export default App;
