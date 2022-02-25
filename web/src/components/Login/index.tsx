aa38c910e994b0e6978ad3f461ea8bfbf756a321
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-02-26 00:12:18 +0200

    Connect login form to backend

--- web/src/components/Login/index.tsx
+++ web/src/components/Login/index.tsx
@@ -3,15 +3,38 @@ import {
   Button,
   Flex,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Heading,
+  IconButton,
   Input,
+  InputGroup,
+  InputRightElement,
+  Link,
   Stack,
   Text,
   useColorModeValue,
+  useToast,
 } from "@chakra-ui/react";
+import { Field, Form, Formik } from "formik";
+import { useState } from "react";
+import { FiEye, FiEyeOff } from "react-icons/fi";
+import { object, string } from "yup";
+import UserService from "../../services/UserService";
+
+const loginValidationSchema = object().shape({
+  username: string()
+    .required("Required")
+    .matches(/^(?!\s+$).*/, "Required"),
+  password: string().required("Required"),
+});
 
 const Login = () => {
+  // const navigate = useNavigate();
+  const toast = useToast();
+
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <Flex
       minH={"100vh"}
@@ -21,9 +44,8 @@ const Login = () => {
     >
       <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
         <Stack align={"center"}>
-          <Heading fontSize={"4xl"}>Login to your account</Heading>
+          <Heading fontSize={"4xl"}>Log in to your account</Heading>
           <Text fontSize={"lg"} color={"gray.600"}>
-            {" "}
             to connect with the world ✌️
           </Text>
         </Stack>
@@ -33,28 +55,93 @@ const Login = () => {
           boxShadow={"lg"}
           p={8}
         >
-          <Stack spacing={4}>
-            <FormControl id="username">
-              <FormLabel>Username</FormLabel>
-              <Input type="text" />
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel>Password</FormLabel>
-              <Input type="password" />
-            </FormControl>
-            <Stack spacing={10} pt={2}>
-              <Button
-                size="lg"
-                bg={"blue.400"}
-                color={"white"}
-                _hover={{
-                  bg: "blue.500",
-                }}
-              >
-                Login
-              </Button>
-            </Stack>
-          </Stack>
+          <Formik
+            initialValues={{ username: "", password: "" }}
+            validationSchema={loginValidationSchema}
+            validateOnChange={false}
+            onSubmit={async (data, { setSubmitting }) => {
+              setSubmitting(true);
+
+              const res = await UserService.login(data.username, data.password);
+
+              if (res.error) {
+                toast({
+                  title: res.message,
+                  status: "error",
+                  isClosable: true,
+                });
+              }
+
+              setSubmitting(false);
+
+              // navigate("/");
+            }}
+          >
+            {({ errors, isSubmitting }) => (
+              <Form>
+                <Stack spacing={4}>
+                  <Field name="username">
+                    {({ field }: { field: unknown }) => (
+                      <FormControl isInvalid={errors.username !== undefined}>
+                        <FormLabel htmlFor="username">Username</FormLabel>
+                        <Input {...field} id="username" />
+                        <FormErrorMessage>{errors.username}</FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
+                  <Field name="password">
+                    {({ field }: { field: unknown }) => (
+                      <FormControl isInvalid={errors.password !== undefined}>
+                        <FormLabel htmlFor="password">Password</FormLabel>
+                        <InputGroup>
+                          <Input
+                            {...field}
+                            id="password"
+                            type={showPassword ? "text" : "password"}
+                          />
+                          <InputRightElement h={"full"}>
+                            <IconButton
+                              variant={"ghost"}
+                              onClick={() =>
+                                setShowPassword(
+                                  (showPasswordState) => !showPasswordState
+                                )
+                              }
+                              aria-label="show password toggle"
+                              icon={showPassword ? <FiEye /> : <FiEyeOff />}
+                            />
+                          </InputRightElement>
+                        </InputGroup>
+                        <FormErrorMessage>{errors.password}</FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
+                  <Stack spacing={10} pt={2}>
+                    <Button
+                      size="lg"
+                      bg={"blue.400"}
+                      color={"white"}
+                      _hover={{
+                        bg: "blue.500",
+                      }}
+                      type="submit"
+                      isLoading={isSubmitting}
+                    >
+                      Login
+                    </Button>
+                  </Stack>
+                  <Stack pt={6}>
+                    <Text align={"center"}>
+                      Don&apos;t have an account?{" "}
+                      <Link color={"blue.400"} href="/register">
+                        Register
+                      </Link>
+                    </Text>
+                  </Stack>
+                </Stack>
+              </Form>
+            )}
+          </Formik>
         </Box>
       </Stack>
     </Flex>
--- web/src/services/UserService.tsx
+++ web/src/services/UserService.tsx
@@ -19,8 +19,8 @@ const UserService = {
   ): Promise<UseToastOptions> => {
     try {
       await axios.post(`${process.env.REACT_APP_API_URL}/api/users/register`, {
-        username,
-        password,
+        username: username.trim(),
+        password: password.trim(),
       });
       return { title: `Registered user "${username}"`, status: "success" };
     } catch (err) {
@@ -35,6 +35,36 @@ const UserService = {
       }
 
       return { title: "Could not register user", status: "error" };
+    }
+  },
+  login: async (
+    username: string,
+    password: string
+  ): Promise<{ message: string; error: boolean }> => {
+    try {
+      const { data } = await axios.post(
+        `${process.env.REACT_APP_API_URL}/api/users/login`,
+        {
+          username: username.trim(),
+          password: password.trim(),
+        }
+      );
+
+      window.localStorage.setItem("nitoUserToken", data.token);
+
+      return { message: "Logged in", error: false };
+    } catch (err) {
+      const error = err as AxiosError;
+
+      if (error.response) {
+        const errorMessage: string = error.response.data.error;
+        const errorMessageCapitalized =
+          errorMessage.charAt(0).toUpperCase() + errorMessage.slice(1);
+
+        return { message: errorMessageCapitalized, error: true };
+      }
+
+      return { message: "Could not log in", error: true };
     }
   },
 };
