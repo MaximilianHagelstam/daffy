6fc25fb309ec25d67caa13e52a1b9458bbf9b8b4
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-02-25 19:16:20 +0200

    Setup register form state handling

--- web/src/components/Register/index.tsx
+++ web/src/components/Register/index.tsx
@@ -18,7 +18,14 @@ import { useState } from "react";
 import { FiEye, FiEyeOff } from "react-icons/fi";
 
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+
+  const handleSubmit = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    console.log(username, password);
+  };
 
   return (
     <Flex
@@ -39,16 +46,26 @@ const Register = () => {
           bg={useColorModeValue("white", "gray.700")}
           boxShadow={"lg"}
           p={8}
+          as="form"
+          onSubmit={handleSubmit}
         >
           <Stack spacing={4}>
             <FormControl id="username" isRequired>
-              <FormLabel>Username</FormLabel>
-              <Input type="text" />
+              <FormLabel htmlFor="username">Username</FormLabel>
+              <Input
+                type="text"
+                id="username"
+                onChange={(e) => setUsername(e.target.value)}
+              />
             </FormControl>
             <FormControl id="password" isRequired>
-              <FormLabel>Password</FormLabel>
+              <FormLabel htmlFor="password">Password</FormLabel>
               <InputGroup>
-                <Input type={showPassword ? "text" : "password"} />
+                <Input
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
                 <InputRightElement h={"full"}>
                   <IconButton
                     variant={"ghost"}
@@ -69,6 +86,7 @@ const Register = () => {
                 _hover={{
                   bg: "blue.500",
                 }}
+                type="submit"
               >
                 Register
               </Button>
