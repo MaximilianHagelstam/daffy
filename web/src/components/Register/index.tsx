4dea93c9eabdb87eb8a0a72f7e6ca01dc63e9acd
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-02-25 17:58:53 +0200

    Design login and register components

--- web/src/components/Register/index.tsx
+++ web/src/components/Register/index.tsx
@@ -1,5 +1,91 @@
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  IconButton,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Link,
+  Stack,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { FiEye, FiEyeOff } from "react-icons/fi";
+
 const Register = () => {
-  return <div>Register</div>;
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"}>Register a new account</Heading>
+          <Text fontSize={"lg"} color={"gray.600"}>
+            to connect with the world ✌️
+          </Text>
+        </Stack>
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
+          <Stack spacing={4}>
+            <FormControl id="username" isRequired>
+              <FormLabel>Username</FormLabel>
+              <Input type="text" />
+            </FormControl>
+            <FormControl id="password" isRequired>
+              <FormLabel>Password</FormLabel>
+              <InputGroup>
+                <Input type={showPassword ? "text" : "password"} />
+                <InputRightElement h={"full"}>
+                  <IconButton
+                    variant={"ghost"}
+                    onClick={() =>
+                      setShowPassword((showPasswordState) => !showPasswordState)
+                    }
+                    aria-label="show password toggle"
+                    icon={showPassword ? <FiEye /> : <FiEyeOff />}
+                  />
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+            <Stack spacing={10} pt={2}>
+              <Button
+                size="lg"
+                bg={"blue.400"}
+                color={"white"}
+                _hover={{
+                  bg: "blue.500",
+                }}
+              >
+                Register
+              </Button>
+            </Stack>
+            <Stack pt={6}>
+              <Text align={"center"}>
+                Already a user?{" "}
+                <Link color={"blue.400"} href="/login">
+                  Login
+                </Link>
+              </Text>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
 };
 
 export default Register;
