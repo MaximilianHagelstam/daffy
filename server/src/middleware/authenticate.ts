a9e1e8ec69e644db6a9833bb1b3f20c21e29232d
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-02-27 13:48:56 +0200

    Handle invalid token error

--- server/src/middleware/authenticate.ts
+++ server/src/middleware/authenticate.ts
@@ -11,17 +11,23 @@ const getRequestToken = (req: Request): string | null => {
 };
 
 const authenticate = (req: Request, res: Response, next: NextFunction) => {
-  const token = getRequestToken(req);
-  if (!token) {
-    return res.status(401).json({
-      error: "missing or invalid token",
+  try {
+    const token = getRequestToken(req);
+    if (!token) {
+      return res.status(401).json({
+        error: "missing token",
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.token = decoded as Token;
+
+    return next();
+  } catch (err) {
+    res.status(401).json({
+      error: "invalid token",
     });
   }
-
-  const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  req.token = decoded as Token;
-
-  return next();
 };
 
 export default authenticate;
