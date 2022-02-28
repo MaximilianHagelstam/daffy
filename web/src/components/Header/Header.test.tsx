dcf5a8df5a3e9df3b92f82e79ef2e06509c03fce
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-02-28 12:04:49 +0200

    Improve CI linting

--- web/src/components/Header/Header.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Header from ".";
-
-test("renders hello header", () => {
-  render(<Header />);
-  const headerElement = screen.getByText(/hello/i);
-  expect(headerElement).toBeInTheDocument();
-});
--- web/src/components/Header/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Heading } from "@chakra-ui/react";
-
-const Header = () => {
-  return <Heading>Hello</Heading>;
-};
-
-export default Header;
