596991138d55c40e1ef2af326cf975681cb3a655
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-03-02 18:24:05 +0200

    Improve backend pagination

--- server/src/controllers/postController.ts
+++ server/src/controllers/postController.ts
@@ -2,12 +2,14 @@ import { Request, Response } from "express";
 import Post from "../entities/Post";
 
 const findAll = async (req: Request, res: Response) => {
-  const limit: number = Number(req.query.limit) || 20;
+  const perPage: number = Number(req.query.perPage) || 5;
+  const page: number = Number(req.query.page) || 1;
 
   const posts = await Post.find({
     relations: ["creator"],
     order: { createdAt: "DESC" },
-    take: limit,
+    take: perPage,
+    skip: perPage * (page - 1),
   });
   return res.json({ posts });
 };
