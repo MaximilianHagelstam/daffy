db7cec152b80af9f8fc886c936028ccd2183eec5
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-03-06 21:30:55 +0200

    Polish user controller

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,58 +1,68 @@
 import bcrypt from "bcrypt";
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import logger from "../config/logger";
 import User from "../entities/User";
 import getRandomAvatarUrl from "../utils/getRandomAvatarUrl";
 
 const register = async (req: Request, res: Response) => {
-  const requestBody = req.body as User;
-  const { username, password } = requestBody;
+  try {
+    const { username, password } = req.body as User;
 
-  const userWithSameUsername = await User.findOne({ where: { username } });
-  if (userWithSameUsername) {
-    return res.status(400).json({ error: "username taken" });
+    const userWithSameUsername = await User.findOne({ where: { username } });
+    if (userWithSameUsername)
+      return res.status(400).json({ error: "username taken" });
+
+    const passwordHash = await bcrypt.hash(password, 10);
+    const avatar = getRandomAvatarUrl();
+
+    const user = await User.create({
+      username,
+      password: passwordHash,
+      avatar,
+    }).save();
+
+    const userWithoutPassword = {
+      id: user.id,
+      username: user.username,
+      avatar: user.avatar,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    };
+
+    logger.info("Created user", user);
+    return res.status(201).json({ user: userWithoutPassword });
+  } catch (err) {
+    logger.error(`Error registering user: ${err}`);
+    return res.status(400).json({ error: "error registering user" });
   }
-
-  const passwordHash = await bcrypt.hash(password, 10);
-  const avatar = getRandomAvatarUrl();
-
-  await User.create({
-    username,
-    password: passwordHash,
-    avatar,
-  }).save();
-
-  return res.status(201).json({ message: "user created" });
 };
 
 const login = async (req: Request, res: Response) => {
-  const requestBody = req.body as User;
-  const { username, password } = requestBody;
+  try {
+    const { username, password } = req.body as User;
 
-  const user = await User.findOne({
-    where: { username },
-    select: ["id", "username", "password"],
-  });
+    const user = await User.findOne({
+      where: { username },
+      select: ["id", "username", "password"],
+    });
+    if (!user)
+      return res.status(401).json({ error: "invalid username or password" });
 
-  if (!user) {
-    return res.status(401).json({ error: "invalid username or password" });
+    const passwordIsCorrect = await bcrypt.compare(password, user.password);
+    if (!passwordIsCorrect)
+      return res.status(401).json({ error: "invalid username or password" });
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    logger.info("Logged in user", user);
+    return res.json({ token });
+  } catch (err) {
+    logger.error(`Error logging in: ${err}`);
+    return res.status(400).json({ error: "error logging in" });
   }
-
-  const passwordIsCorrect = await bcrypt.compare(password, user.password);
-  if (!passwordIsCorrect) {
-    return res.status(401).json({ error: "invalid username or password" });
-  }
-
-  const token = jwt.sign(
-    {
-      username: user.username,
-      id: user.id,
-    },
-    process.env.JWT_SECRET,
-    { expiresIn: "7d" }
-  );
-
-  return res.json({ token });
 };
 
 const getCurrentUser = async (req: Request, res: Response) => {
