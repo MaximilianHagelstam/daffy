8e76f77ff672e535da3320ffd66c120e00a2e933
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-03-08 12:11:32 +0200

    Design search bar

--- web/src/components/Home/index.tsx
+++ web/src/components/Home/index.tsx
@@ -1,3 +1,5 @@
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { FiSearch } from "react-icons/fi";
 import PostService from "../../services/PostService";
 import PostList from "../PostList";
 import CreateButton from "./CreateButton";
@@ -19,6 +21,12 @@ const Home = () => {
           console.log("popular");
         }}
       /> */}
+      <InputGroup colorScheme="purple" maxW="400px" mb={8}>
+        <InputLeftElement pointerEvents="none">
+          <FiSearch />
+        </InputLeftElement>
+        <Input type="text" placeholder="Search" focusBorderColor="purple.400" />
+      </InputGroup>
       <PostList fetchFunction={PostService.getAll} />
       <CreateButton />
     </>
